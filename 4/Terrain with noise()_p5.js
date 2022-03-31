2f390c91ff90f937c8ed56e171a638c4689731a6
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-03-31 12:45:00 +0900

    Update Terrain with noise()_p5.js

--- 4/Terrain with noise()_p5.js
+++ 4/Terrain with noise()_p5.js
@@ -1,2 +1,22 @@
 //4. terrian 관련 코딩
 //JavaScript에서 2D에 noise() 값 넣기
+
+
+function setup() {
+  createCanvas(800, 800);
+  noStroke();
+  background(0);
+}
+
+function draw() {
+  for (var x = 0; x < 100; x++)
+  {
+    for (var y = 0; y< 100; y++) {
+      var n = noise (x/8, y/8);
+      var brightness = map( n, 0, 1, 0, 255);
+      var size = map(n, 0, 1, 5, 40);
+      fill(brightness - size/n, brightness*size/7, brightness-size);
+      ellipse(x*40, y * 30, size, size);
+    }
+  }
+}
